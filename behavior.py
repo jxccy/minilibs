7b48597d3899a4b98ec3301d06703f6051fc9609
He <xiaotonghe@newbanker.cn>
2018-10-12 15:21:55 +0800

    十万用户量  1亿文章量

--- behavior.py
+++ behavior.py
@@ -1,29 +1,21 @@
 # -*- coding:utf-8 -*-
 import json
 import random
-import re
+from time import sleep
 
 import requests
 
-def get_userid():
-    return str(int(random.random()*100000000))
+get_userid = lambda: str(int(random.random() * 10 ** 6))
+get_itemid = lambda: str(int(random.random() * 10 ** 9))
+get_score = lambda: str(random.choice(range(10)))
 
-def get_itemid():
-    def get_str():
-        lst = re.findall('\w', 'qwertyuiopasdfghjklzxcvbnm1234567890')
-        return random.choice(lst)
-    return ''.join([get_str() for i in range(10)])
-
-def get_score():
-    return random.choice(re.findall('\w', '1234567890'))
 
 def get_body():
-    userid = get_userid()
-    itemid = get_itemid()
-    res = [userid, itemid]
+    res = [get_userid(), get_itemid()]
     for i in range(10):
         res.append(get_score())
     return '|'.join(res)
+
 
 def request():
     headers = {"h1": "v1", "h2": "v2"}
@@ -31,5 +23,8 @@ def request():
     url = 'http://192.168.0.161:50000'
     requests.post(url=url, data=json.dumps(data))
 
+
 if __name__ == '__main__':
-    request()
+    while True:
+        request()
+        sleep(1)
